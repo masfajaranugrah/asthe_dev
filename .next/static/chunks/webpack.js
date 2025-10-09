8990876aca118068859a743367333cb9caec2b85
fajar anugrah <fajaranugrah2203@gmail.com>
2025-10-09 23:55:43 +0700

    fetch: update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cccd711d6581bb10"; }
+/******/ 		__webpack_require__.h = function() { return "d6149bf3b2c5252c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
